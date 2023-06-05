efc9c8cd3e5ace92d63543b039d72f8c0f6a599b
Alex <oalexsanderm@gmail.com>
2023-06-05 13:26:50 -0300

    Arrumando o tipo data em data de nascimento

--- OrcamentoWeb/Models/ClienteModel.cs
+++ OrcamentoWeb/Models/ClienteModel.cs
@@ -1,13 +1,20 @@
-﻿namespace OrcamentoWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrcamentoWeb.Models
 {
 	public class ClienteModel
 	{
         public int Id { get; set; }
         public string Nome { get; set; }
         public long Cpf { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime DataNascimento { get; set; }
         public string Email { get; set; }
         public long Telefone { get; set; }
         public string Endereco { get; set; }
+
     }
+
+
 }
